6ae3e5aa160cec4bb5a2752305f7f115c33a39d3
Baek Young Hoon <yyh0965@gmail.com>
2023-08-15 16:37:09 +0900

    기본 틀 만드는중
    TabLayout 및 Fragment 사용

--- app/src/main/java/com/hoon/AuctionStore/DB.kt
+++ app/src/main/java/com/hoon/AuctionStore/DB.kt
@@ -1,6 +1,7 @@
 package com.hoon.AuctionStore
 import android.app.Application
 import com.google.firebase.FirebaseApp
+import com.google.firebase.database.FirebaseDatabase
 
 val database = FirebaseDatabase.getInstance()
 class DB : Application() {
--- app/src/main/java/com/hoon/AuctionStore/MainActivity.kt
+++ app/src/main/java/com/hoon/AuctionStore/MainActivity.kt
@@ -1,20 +1,49 @@
 package com.hoon.AuctionStore
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity() {
+    private var tabs: TabLayout? = null
+    private val fragmentManager: FragmentManager = supportFragmentManager
+    private lateinit var fragment1: Fragment1
+    private lateinit var fragment2: Fragment2
+    private lateinit var fragment3: Fragment3
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnLay1: Button = findViewById<Button>(R.id.btnLay1)
+        fragment1 = Fragment1()
+        fragment2 = Fragment2()
+        fragment3 = Fragment3()
 
-        btnLay1.setOnClickListener(){
-            val intent = Intent(this, Lay1Activity::class.java)
-            startActivity(intent)
+        fragmentManager.beginTransaction().add(R.id.container, fragment1).commit()
+
+        tabs = findViewById(R.id.tabs)
+        tabs?.apply {
+            addTab(newTab().setText("친구"))
+            addTab(newTab().setText("채팅"))
+            addTab(newTab().setText("더보기"))
+            addOnTabSelectedListener(object : OnTabSelectedListener {
+                override fun onTabSelected(tab: TabLayout.Tab) {
+                    val position = tab.position
+                    val selected: Fragment = when (position) {
+                        0 -> fragment1
+                        1 -> fragment2
+                        2 -> fragment3
+                        else -> fragment1
+                    }
+                    fragmentManager.beginTransaction().replace(R.id.container, selected).commit()
+                }
+
+                override fun onTabUnselected(tab: TabLayout.Tab) {}
+                override fun onTabReselected(tab: TabLayout.Tab) {}
+            })
         }
     }
 }
