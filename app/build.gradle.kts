442e04f67bfa5243cb225198b62287e06c9bf884
Baek Young Hoon <yyh0965@gmail.com>
2023-08-29 20:41:33 +0900

    컴포즈 실험 중
    의존성 추가
    프래그먼트에서 쓰는중

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -8,14 +10,18 @@ plugins {
 detekt {
     autoCorrect = true
 }
-
 android {
     namespace = "com.hoon.AuctionStore"
-    compileSdk = 33
+    compileSdk = 34
+
+    buildFeatures {
+        // Enables Jetpack Compose for this module
+        compose = true
+    }
 
     defaultConfig {
         applicationId = "com.hoon.AuctionStore"
-        minSdk = 29
+        minSdk = 33
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
@@ -39,6 +45,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.0"
+    }
     kotlinOptions {
         jvmTarget = "1.8"
     }
@@ -59,10 +68,32 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
+    implementation("androidx.compose.ui:ui-tooling-preview-android:1.5.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.google.android.material:material:1.1.0")
 
     detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.1")
+
+    implementation ("androidx.compose.ui:ui:1.0.0-beta07")
+    // Tooling support (Previews, etc.)
+    implementation ("androidx.compose.ui:ui-tooling:1.0.0-beta07")
+    // Foundation (Border, Background, Box, Image, Scroll, shapes, animations, etc.)
+    implementation ("androidx.compose.foundation:foundation:1.0.0-beta07")
+    // Material Design
+    implementation ("androidx.compose.material:material:1.0.0-beta07")
+    // Material design icons
+    implementation ("androidx.compose.material:material-icons-core:1.0.0-beta07")
+    implementation ("androidx.compose.material:material-icons-extended:1.0.0-beta07")
+    // Integration with activities
+    implementation ("androidx.activity:activity-compose:1.3.0-alpha08")
+    // Integration with ViewModels
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha05")
+    // Integration with observables
+    implementation ("androidx.compose.runtime:runtime-livedata:1.0.0-beta07")
+    implementation ("androidx.compose.runtime:runtime-rxjava2:1.0.0-beta07")
+
+    // UI Tests
+    androidTestImplementation ("androidx.compose.ui:ui-test-junit4:1.0.0-beta07")
 }
--- app/src/main/java/com/hoon/AuctionStore/Fragment_Chat.kt
+++ app/src/main/java/com/hoon/AuctionStore/Fragment_Chat.kt
@@ -1,9 +1,28 @@
 package com.hoon.AuctionStore
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 
 class Fragment_Chat : Fragment() {
@@ -12,6 +31,65 @@ class Fragment_Chat : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.chat_fragment, container, false) as ViewGroup
+        return ComposeView(requireContext()).apply {
+            // Dispose of the Composition when the view's LifecycleOwner
+            // is destroyed
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                Greeting("fuck!")
+                exgam()
+                MaterialTheme {
+                    // In Compose world
+                    Text("Hello 한글!")
+
+                }
+            }
+        }
     }
+}
+@Composable
+fun Greeting(name: String){
+    MaterialTheme{
+        Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Blue) // 여기에서 원하는 색상을 지정하세요.
+    )
+        Text(text = "hello 안녕 $name")
+    }
+
+
+}
+@Composable
+fun exgam(){
+    MaterialTheme {
+        val typography = MaterialTheme.typography
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = "A day wandering through the sandhills " +
+                        "in Shark Fin Cove, and a few of the " +
+                        "sights I saw",
+                style = typography.h6,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(
+                text = "Davenport, California",
+                style = typography.body2
+            )
+            Text(
+                text = "December 2018",
+                style = typography.body2
+            )
+        }
+    }
+}
+@Preview
+@Composable
+fun PreviewGreeting(){
+    Greeting("병신")
 }
